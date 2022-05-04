377b6bd6620101239eb016469e10b852b5ab60ed
NicolaSilviu <50529770+NicolaSilviu@users.noreply.github.com>
2022-05-04 08:54:45 +0300

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,10 +1,10 @@
 for number in range(100, 0, -1):
     if number % 5 == 0 and number % 3 == 0:
-        print("Agile")
+        print("Testing")
     elif number % 3 == 0:
         print("Software")
     elif number % 5 == 0:
-        print("Testing")
+        print("Agile")
     else:
         print(number)
         
